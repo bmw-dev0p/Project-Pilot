a792f2fe83b7b00e88d600200096b3a7274e114c
bmw-dev0p <bmw.dev0p@gmail.com>
2024-09-17 22:01:58 -0400

    completed sign in feature, along with error messages for blank fields + incorrect login

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -2,8 +2,8 @@ import { User } from '../models/userFactory.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
-    { username: 'JollyGuru', email: 'jolly@guru.com', password: 'password' },
-    { username: 'SunnyScribe', email: 'sunny@scribe.com', password: 'password' },
-    { username: 'RadiantComet', email: 'radiant@comet.com', password: 'password' },
+    { fname: 'Jolly', lname: 'Guru', username: 'JollyGuru', email: 'jolly@guru.com', password: 'password' },
+    { fname: 'Sunny', lname: 'Scribe', username: 'SunnyScribe', email: 'sunny@scribe.com', password: 'password' },
+    { fname: 'Radiant', lname: 'Comet', username: 'RadiantComet', email: 'radiant@comet.com', password: 'password' },
   ], { individualHooks: true });
 };
