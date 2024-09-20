e420da2af00bda729000d840f6febbf83d500353
andrew-surrena <andrewsurrena@gmail.com>
2024-09-19 19:38:03 -0500

    added task & status routes

--- server/src/routes/api/api-routes.ts
+++ server/src/routes/api/api-routes.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import authRoutes from './auth-routes.js';
 import { authenticateToken } from '../../config/authToken.js';
 import { userRouter } from './user-routes.js';
-
+import { taskRouter } from './task-routes.js';
+import { statusRouter } from './status-routes.js';
 
 const router = Router();
 
@@ -11,5 +12,7 @@ const apiRoutes = router.use('users', userRouter);
 router.use('/auth', authRoutes);
 router.use('/api', authenticateToken, apiRoutes);
 router.use('/users', userRouter);
+router.use('/tasks', taskRouter);
+router.use('/status', statusRouter);
 
 export default router;
